f2462d7317e8a0404c2b09fc8bd467c4eeb0f5ef
Mike Luken <mike.luken@gmail.com>
2025-08-22 15:51:48 -0500

    launch modal bootstrap test

--- Scripts/errors.ts
+++
@@ -1,3 +0,0 @@
-﻿export function hideErrorMessage(): void {
-    console.log("Form submitted...");
-}
--- Scripts/site.ts
+++ Scripts/site.ts
@@ -1,55 +1,18 @@
-﻿import { hideErrorMessage } from './errors.js';
-
-export function formSubmitted(result: unknown): void {
-    hideErrorMessage();
-}
-
-function formSubmitting(): void {
-    console.log("form submitting...");
-
-    let button = document.getElementById('btnSubmit') as HTMLButtonElement;
-    button.disabled = true;
-}
-
-window.addEventListener('pagehide', () => {
-    const form = document.getElementById("f1") as HTMLFormElement;
-    if (form) {
-        console.log("removing submit event listener");
-        form.removeEventListener('submit', async (event) => { });
-    }
-});
+﻿import * as bootstrap from 'bootstrap';
 
 document.addEventListener('DOMContentLoaded', (event) => {
-    const form = document.getElementById("f1") as HTMLFormElement;
-
-    if (form) {
-
-        form.addEventListener('submit', async (event) => {
-            event.preventDefault();
-
-            formSubmitting();
-            hideErrorMessage();
-
-            if (form.checkValidity()) {
-                const formData = new FormData(form as HTMLFormElement);
-
-                const response = await fetch('/index', {
-                    method: 'POST',
-                    body: formData
-                });
-
-                if (response.ok) {
-                    const resultData = await response.json();
-                    console.log(resultData);
-                } else {
-                    const errorData = await response.json();
-                    console.error('Form submission failed:', errorData);
-                }
-            } else {
-                let button = document.getElementById('btnSubmit') as HTMLButtonElement;
-                button.disabled = false;
-            }
+    const btnLaunchModal = document.getElementById('btnLaunchModal');
+    if (btnLaunchModal) {
+        btnLaunchModal.addEventListener('click', async (event) => {
+            launchModal();
         });
     }
 });
 
+function launchModal() {
+    let myModalElement = document.getElementById('myModal');
+    if (myModalElement) {
+        let modal = new bootstrap.Modal(myModalElement);
+        modal.show();
+    }
+}
